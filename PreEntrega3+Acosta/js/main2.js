dd592a1506b805aa5c76f214f6286ca5329b0b54
Nahuel <117119383+acostanahuel@users.noreply.github.com>
2022-12-11 19:56:06 -0300

    Add files via upload

---
+++ PreEntrega3+Acosta/js/main2.js
@@ -0,0 +1,83 @@
+const listado = document.getElementById("listado");
+let botonesAgregar= document.querySelectorAll(".producto-agregar");
+let acumulador= document.querySelector("#acumulador");
+
+
+const listadoProductos= "json/stock.json";
+
+
+
+fetch(listadoProductos)
+   .then (resp => resp.json())
+   .then (stock => {
+         stock.forEach(producto => {
+            listado.innerHTML += `
+                        <img class="producto-imagen" src="${producto.imagen}" alt="${producto.sector}">
+                         <div class="producto-detalles">
+                         <h3 class="producto-titulo">${producto.sector}</h3>
+                        <p class="producto-precio">$${producto.precio}</p>
+                        <button class="producto-agregar" id="${producto.id}"> Agregar a carrito </button>
+                         </div>
+                        `
+             });
+             
+             actualizarBotonesAgregar ()
+             
+});
+
+
+
+
+
+/////mis funciones cuando estaba cargando productos por array
+
+
+function actualizarBotonesAgregar () {
+   botonesAgregar = document.querySelectorAll(".producto-agregar");
+
+   botonesAgregar.forEach (boton => {
+       boton.addEventListener("click", agregarAlCarrito); 
+   });
+}
+
+let productosEnCarrito;
+
+let productosEnCarritoLS= localStorage.getItem("productos-en-carrito");
+
+
+
+if (productosEnCarritoLS) {
+   productosEnCarrito= JSON.parse(productosEnCarritoLS);
+   actualizarAcumulador();
+  
+} else {
+   productosEnCarrito = [];
+}
+
+function agregarAlCarrito (e) {
+  
+   const idBoton= e.currentTarget.id;
+   const productoAgregado = productos.find (producto => producto.id === idBoton);
+  
+   if (productosEnCarrito.some (producto => producto.id === idBoton)) {
+      const index = productosEnCarrito.findIndex (producto => producto.id === idBoton);
+      productosEnCarrito[index].cantidad++;
+
+   } else {
+       productoAgregado.cantidad= 1; 
+       productosEnCarrito.push(productoAgregado);
+   } 
+
+   actualizarAcumulador();
+  
+   localStorage.setItem("productos-en-carrito", JSON.stringify(productosEnCarrito));
+}       
+
+function actualizarAcumulador(){
+   let nuevoAcumulador = productosEnCarrito.reduce ((acc, producto) => acc + producto.cantidad, 0);
+   acumulador.innerText = nuevoAcumulador;
+}
+
+
+
+
